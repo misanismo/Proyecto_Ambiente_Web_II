0fc61d88e5beb0dd4b2e3cd30ed01e747cfd453c
misanismo <misa93@hotmail.es>
2015-02-23 20:11:49 -0600

    Version 1.0
    -Logueo de seccion administrativa.
    -Registra, Edita, Elimina Usuarios.

---
+++ mvc5/Models/AccountViewModels.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC5.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+    }
+
+    public class ManageUserViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña anterior:")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña nueva:")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar nueva contraseña:")]
+        [Compare("NewPassword", ErrorMessage = "La contraseña nueva no coincide con la verificación")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Nombre de usuario:")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña:")]
+        public string Password { get; set; }
+
+        [Display(Name = "Recordarme?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Nombre de usuario:")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña:")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña:")]
+        [Compare("Password", ErrorMessage = "Las contraseña ingresadas no coinciden")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ mvc5/Models/UsuarioModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace MVC5.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Nombre_Usuario { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Contrasenna { get; set; }
+    }
+
+    public class dbSistemaNutricion : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
